0a7f07c07c9b29d44a78cac2af84a71d50114483
olivia99 <jieying.yang1@gmail.com>
2024-11-08 17:42:05 +0800

    added costum and overall clothing

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 const hairFiles = ["long_hair_front_003.png", "long_hair_front_004.png", "long_hair_front_006.png","long_hair_front_007.png","long_hair_front_008.png","long_hair_front_009.png","long_hair_front_010.png","long_hair_front_012.png","long_hair_front_013.png","long_hair_front_014.png","long_hair_front_015.png","long_hair_front_016.png","long_hair_front_017.png","long_hair_front_018.png","long_hair_front_019.png","long_hair_front_020.png"];
 const hairBackFiles = Array.from({ length: 6 }, (_, i) => `long_hair_back_${String(i + 1).padStart(2, '0')}.png`);
 const bodyFiles = Array.from({ length: 50 }, (_, i) => `${i + 1}.png`);
-const overallFiles = Array.from({ length: 136 }, (_, i) => `one_piece_${String(i + 2).padStart(3, '0')}.png`);
+const overallFiles = Array.from({ length: 11 }, (_, i) => `overall_${String(i + 1).padStart(3, '0')}.png`);
 const shoesFiles = Array.from({ length: 86 }, (_, i) => `shoes_${String(i + 1).padStart(3, '0')}.png`);
 const botFiles = Array.from({ length: 123 }, (_, i) => `bot_${String(i + 1).padStart(3, '0')}.png`);
 const topFiles = Array.from({ length: 138 }, (_, i) => `top_${String(i + 1).padStart(3, '0')}.png`);
+const costumFiles = Array.from({ length: 19 }, (_, i) => `costum_${String(i + 1).padStart(3, '0')}.png`);
 // Global variables to store the images after they are loaded
-let hairImages, hairBackImages, bodyImages, pieceImages, shoeImages;
+//let hairImages, hairBackImages, bodyImages, pieceImages, shoeImages;
 
 // Function to load images from a directory
 async function loadImagesFromDirectory(directoryPath, filenames) {
@@ -29,19 +30,23 @@ async function loadAndCombineImages() {
     const hairDirectory = "assets/hair_front";
     const hairBackDirectory = "assets/hair_back";
     const bodyDirectory = "assets/body";
-    const pieceDirectory = "assets/overall";
+   
     const shoesDirectory = "assets/shoes";
     const botDirectory = "assets/bot";
     const topDirectory = "assets/top"
+    const overallDirectory = "assets/overall"
+    const costumDirectory = "assets/costum"
 
     // Load images from each category
     hairImages = await loadImagesFromDirectory(hairDirectory, hairFiles);
     hairBackImages = await loadImagesFromDirectory(hairBackDirectory, hairBackFiles);
     bodyImages = await loadImagesFromDirectory(bodyDirectory, bodyFiles);
-    overallImages = await loadImagesFromDirectory(pieceDirectory, overallFiles);
+    overallImages = await loadImagesFromDirectory(overallDirectory, overallFiles);
     shoesImages = await loadImagesFromDirectory(shoesDirectory, shoesFiles);
     botImages = await loadImagesFromDirectory(botDirectory,botFiles);
     topImages = await loadImagesFromDirectory(topDirectory,topFiles);
+    costumImages = await loadImagesFromDirectory(costumDirectory,costumFiles);
+
 
     // Initial shuffle to apply images to the page
     shuffleCharacterAssets();
@@ -63,14 +68,17 @@ function shuffleCharacterAssets() {
     const selectedShoes = shuffleArray(shoesImages);
     const selectedTop = shuffleArray(topImages);
     const selectedBot = shuffleArray(botImages);
+    const selectedCostum = shuffleArray(costumImages);
 
     // Update the image sources with the selected random images
     document.querySelector('.hair_front').src = selectedHair.src;
     document.querySelector('.hair_back').src = selectedHairBack.src;
     document.querySelector('.character').src = selectedBody.src;
-    document.querySelector('.top').src = selectedTop.src;
-    document.querySelector('.bot').src = selectedBot.src;
-    document.querySelector('.shoes').src = selectedShoes.src;
+   // document.querySelector('.top').src = selectedTop.src;
+  //  document.querySelector('.bot').src = selectedBot.src;
+  //  document.querySelector('.shoes').src = selectedShoes.src;
+   // document.querySelector('.overall').src = selectedOverall.src;
+    document.querySelector('.costum').src = selectedCostum.src;
 }
 
 // Event listener to trigger the shuffle when the user clicks anywhere on the page
